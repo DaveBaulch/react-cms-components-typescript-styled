003fce464cd343583ab6ea223949cdd5a9f1bfb8
Dave Baulch <davebaulch@yahoo.co.uk>
2020-12-22 12:43:47 +0000

    more refactoring

---
+++ .history/src/components/Button/Button_20201222123836.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Button.scss';
+
+const Button = ({ variant, children }) => {
+  return (
+    <button className={`button ${variant}`}>{children}</button>)
+};
+
+export default Button;
